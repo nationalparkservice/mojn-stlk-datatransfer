5256b4a9f2a0a71202b3da66a0f55ef9b73f9705
Sarah Wright <s.wright.618@gmail.com>
2019-10-21 10:57:44 -0700

    Initial commit

---
+++ data-transfer.R
@@ -0,0 +1,12 @@
+# Download photos using Python script
+
+## Put a copy of photos in incoming photos folder
+
+## Rename photos and put a copy in STLK data folder
+
+
+# Read data from AGOL
+
+# Wrangle data
+
+# Load data into SQL database

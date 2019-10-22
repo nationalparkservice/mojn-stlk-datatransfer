35e4881ec72ca9036b8dd95582910b3d95243bee
Sarah Wright <s.wright.618@gmail.com>
2019-10-22 16:38:07 -0700

    Download benchmark photos

--- data-transfer.R
+++ data-transfer.R
@@ -7,9 +7,20 @@ visit.data <- paste(gdb.path, "STLK_Lake_Annual_Field_Visit", sep = "/")
 photo.data <- paste(gdb.path, "Photos", sep = "/")
 photo.dest <- "M:/MONITORING/StreamsLakes/Data/WY2019/ImageData/Lakes"
 originals.dest <- "M:/MONITORING/_FieldPhotoOriginals_DoNotModify/AGOL_STLK"
+# photo.dest <- "C:/Users/sewright/Desktop/STLKPhotoDownloadTest"
+# originals.dest <- "C:/Users/sewright/Desktop/STLKPhotoDownloadTest/Originals"
 source_python("download-photos.py")
 
-download_photos(attTable = photo.table, photoFeatureClass = photo.data, visitFeatureClass = visit.data, dataPhotoLocation = photo.dest, originalsLocation = originals.dest)
+## Download photos from annual lake visits
+download_visit_photos(attTable = photo.table, photoFeatureClass = photo.data, visitFeatureClass = visit.data, dataPhotoLocation = photo.dest, originalsLocation = originals.dest)
+
+## Download benchmark photos
+gdb.path <- "M:/MONITORING/StreamsLakes/Data/WY2019/FieldData/Lakes_Annual/STLK_LakeLevels_20191022.gdb"
+photo.table <- paste(gdb.path, "BenchPhoto__ATTACH", sep = "/")
+visit.data <- paste(gdb.path, "Form_2", sep = "/")
+photo.data <- paste(gdb.path, "BenchPhoto", sep = "/")
+
+download_bench_photos(attTable = photo.table, photoFeatureClass = photo.data, visitFeatureClass = visit.data, dataPhotoLocation = photo.dest, originalsLocation = originals.dest)
 
 # Read tabular data from AGOL
 

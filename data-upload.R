2d815385f47422109ef588d65f661e52a0082522
Sarah E. Wright <s.wright.618@gmail.com>
2021-02-24 13:00:24 -0800

    Update paths

--- data-upload.R
+++ data-upload.R
@@ -2,13 +2,13 @@
 
 #---------Settings----------#
 # gdb.path <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\FieldData\\Lakes_Annual\\STLK_AnnualLakeVisit_20191022.gdb"
-gdb.path <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\MOJN_STLK_AnnualLakeVisit_Export_20200821.gdb"
-photo.dest <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\ImageData\\Lakes"
-originals.dest <- "M:\\MONITORING\\_FieldPhotoOriginals_DoNotModify\\AGOL_STLK"
-db.params.path <- "C:\\Users\\EEdson\\Desktop\\Projects\\MOJN\\stlk-database-conn.csv"
-#photo.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest"
-#originals.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\Originals"
-#db.params.path <- "C:/Users/sewright/Documents/R/mojn-stlk-datatransfer/stlk-database-conn.csv"
+gdb.path <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\MOJN_STLK_AnnualLakeVisit_20210210.gdb"
+# photo.dest <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\ImageData\\Lakes"
+# originals.dest <- "M:\\MONITORING\\_FieldPhotoOriginals_DoNotModify\\AGOL_STLK"
+# db.params.path <- "C:\\Users\\EEdson\\Desktop\\Projects\\MOJN\\stlk-database-conn.csv"
+photo.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest"
+originals.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\Originals"
+db.params.path <- "C:/Users/sewright/Documents/R/mojn-stlk-datatransfer/stlk-database-conn.csv"
 #---------------------------#
 
 #--------pull tables-----------#

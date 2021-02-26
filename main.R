391ab00d93e3ef5634106d575dd263f6d3a0e45f
Sarah E. Wright <s.wright.618@gmail.com>
2021-02-26 11:08:09 -0800

    Update settings

--- main.R
+++ main.R
@@ -1,12 +1,19 @@
-#---------Settings----------#
-# gdb.path <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\FieldData\\Lakes_Annual\\STLK_AnnualLakeVisit_20191022.gdb"
-gdb.path <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\MOJN_STLK_AnnualLakeVisit_20210224.gdb"
-# photo.dest <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\ImageData\\Lakes"
-# originals.dest <- "M:\\MONITORING\\_FieldPhotoOriginals_DoNotModify\\AGOL_STLK"
-# db.params.path <- "C:\\Users\\EEdson\\Desktop\\Projects\\MOJN\\stlk-database-conn.csv"
-photo.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest"
-originals.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\Originals"
-db.params.path <- "C:/Users/sewright/Documents/R/mojn-stlk-datatransfer/stlk-database-conn.csv"
+# #---------TEST Settings----------#
+# # gdb.path <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\FieldData\\Lakes_Annual\\STLK_AnnualLakeVisit_20191022.gdb"
+# gdb.path <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\MOJN_STLK_AnnualLakeVisit_20210224.gdb"
+# # photo.dest <- "M:\\MONITORING\\StreamsLakes\\Data\\WY2019\\ImageData\\Lakes"
+# # originals.dest <- "M:\\MONITORING\\_FieldPhotoOriginals_DoNotModify\\AGOL_STLK"
+# # db.params.path <- "C:\\Users\\EEdson\\Desktop\\Projects\\MOJN\\stlk-database-conn.csv"
+# photo.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest"
+# originals.dest <- "C:\\Users\\sewright\\Desktop\\STLKPhotoDownloadTest\\Originals"
+# db.params.path <- "C:/Users/sewright/Documents/R/mojn-stlk-datatransfer/stlk-database-conn.csv"
+# #---------------------------#
+
+#---------LIVE Settings----------#
+gdb.path <- "M:\\MONITORING\\StreamsLakes\\Data\\Database\\AGOLBackups\\MOJN_STLK_AnnualLakeVisit_20210224.gdb"
+photo.dest <- "M:\\MONITORING\\StreamsLakes\\Data\\ImageData\\Lakes"
+originals.dest <- "M:\\MONITORING\\_FieldPhotoOriginals_DoNotModify"
+db.params.path <- "LIVE-stlk-database-conn.csv"
 #---------------------------#
 
 Sys.setenv(RETICULATE_PYTHON = "C:/Python27/ArcGISx6410.5")
